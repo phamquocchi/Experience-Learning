ae2c6ac9e45dbdad72f54ab2d913c485f734ddfa
phamquocchi <40203938+phamquocchi@users.noreply.github.com>
2021-10-30 00:05:17 -0500

    Add files via upload

---
+++ Module3.py
@@ -0,0 +1,17 @@
+# Task 1
+print("Hello there!", 5)
+
+# Task 2
+print("Hello there!\n", 5)
+print("Hello there!\t", 5)
+print("\"Java is to JavaScript what car is to Carpet.\" – Chris Heilmann")
+
+# Task 3
+print("Hello" + " " + "there" + "!")
+
+# Task 4
+print("%.3e" % 3.78)
+
+# Task 5
+print(chr(65))
+
